4b520c9fead88b9b1a1c4217d59937b6f372eb00
Natã Mansur <nata.mansur8@gmail.com>
2021-11-29 16:38:53 -0300

    ex das aulas

---
+++ fundamentos/bloco-4-introdução-a-JS-e-lógica-de-prog/dia-2-array-e-loop-for-4.2/listas.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Portfólio') ;
+menu.push('Contato')
+
+// console.log(menuServices);
+// console.log(menu);
+//.length - tamanho
+//.push() - add final
+//.unshift() - add inicio
+//.pop() - apaga o ultimo
+//.shift() - apaga o primeiro
+//.indexOf() - proc
